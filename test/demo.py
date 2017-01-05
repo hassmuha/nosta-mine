72964d254977328e10766c039f3eccf2423725c1
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-05 22:06:43 +0100

    Echo problem solved

--- test/demo.py
+++ test/demo.py
@@ -28,7 +28,9 @@ def handle_messages():
   print payload
   for sender, message in messaging_events(payload):
     print "Incoming from %s: %s" % (sender, message)
-    send_message(PAT, sender, message)
+    # modifid by Hassan : to fix the echo problem. the problem is message echo option is on by default and whenever page send a message to user one more status message follows
+    if message != "I can't echo this" :
+    	send_message(PAT, sender, message)
   return "ok"
 
 def messaging_events(payload):
